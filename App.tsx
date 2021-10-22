6d39dca805ffa1d1aa876c303c48be70f983e400
unknown <manoelprado.aecjr@gmail.com>
2021-10-22 12:00:13 -0300

    using styled-components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Welcome } from './src/Components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome title="React Native Bare Workflow com TypeScript"/>
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
