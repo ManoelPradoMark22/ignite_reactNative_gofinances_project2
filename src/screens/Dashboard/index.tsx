4dac1dd5e470e0cc459c35a3760fac2ed25e7e17
unknown <manoelprado.aecjr@gmail.com>
2021-10-25 14:11:28 -0300

    using proportions of measures - begin style header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/53825592?v=4' }}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Manoel</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
+
     </Container>
   )
 }
