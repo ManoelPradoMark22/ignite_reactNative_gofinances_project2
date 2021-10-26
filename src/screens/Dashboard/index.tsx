e9334ea40fd48c1a7f4dc81d3b3547c4692f5e2f
unknown <manoelprado.aecjr@gmail.com>
2021-10-26 12:13:21 -0300

    finish transactions List - typing FlatList in styledComponents

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { HighlightCard } from '../../components/HighlightCard';
-import { TransactionCard } from '../../components/TransactionCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import {
   Container,
@@ -20,9 +19,14 @@ import {
   TransactionList
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
-  const data = [
+  const data : DataListProps[] = [
     {
+      id: '1',
       type: 'positive',
       title: "Desenvolvimento de site",
       amount: "R$ 12.000,00",
@@ -33,6 +37,7 @@ export function Dashboard() {
       date: "26/10/2021"
     },
     {
+      id: '2',
       type: 'negative',
       title: "Hamburgueria Pizzy",
       amount: "R$ 59,00",
@@ -43,6 +48,7 @@ export function Dashboard() {
       date: "20/10/2021"
     },
     {
+      id: '3',
       type: 'negative',
       title: "Aluguel do apartamento",
       amount: "R$ 1.200,00",
@@ -96,11 +102,8 @@ export function Dashboard() {
 
         <TransactionList 
           data={data}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => <TransactionCard data={item} />}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: getBottomSpace()
-          }}
         />
       </Transactions>
     </Container>
