0e0d072b1bccecc073e07a670aafe612fffb6fdb
unknown <manoelprado.aecjr@gmail.com>
2021-10-27 11:16:50 -0300

    use GestureHandlerRootView instear View in MODAL page - make buttons works propperly

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -2,7 +2,9 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export const Container = styled(GestureHandlerRootView)`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
 `;
