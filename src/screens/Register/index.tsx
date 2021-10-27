fb4af1ed9b57c1cf7b3399eef1a83272b5409bde
unknown <manoelprado.aecjr@gmail.com>
2021-10-27 10:17:56 -0300

    rename CategorySelect component to CategorySelectButton - avoid two equals names

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
-import { CategorySelect } from '../../components/Form/CategorySelect';
+import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
 
 import { 
   Container,
@@ -51,7 +51,7 @@ export function Register() {
             />
           </TransactionsTypes>
 
-          <CategorySelect title="Categoria" />
+          <CategorySelectButton title="Categoria" />
         </Fields>
         <Button title="Enviar"/>
       </Form>
