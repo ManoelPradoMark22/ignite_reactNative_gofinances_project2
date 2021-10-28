f8c62f1cfc8aba0e6b4812b8479034408c4ac096
unknown <manoelprado.aecjr@gmail.com>
2021-10-28 14:00:13 -0300

    useFocusEffect to reload Dashboard when focus

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { useFocusEffect } from '@react-navigation/native';
 
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
@@ -66,6 +68,10 @@ export function Dashboard() {
   useEffect(() => {
     loadTransactions();
   },[]);
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions();
+  },[]));
 
   return (
     <Container>
