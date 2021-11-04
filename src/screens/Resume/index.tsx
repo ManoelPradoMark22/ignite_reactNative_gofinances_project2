782c64c6159fee9d74105d025f8b4ba89a941f1d
Manoel Prado <manoelprado.aecjr@gmail.com>
2021-11-04 17:24:20 -0300

    start create RESUMO page

---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { HistoryCard } from '../../components/HistoryCard';
+
+import {
+  Container,
+  Header,
+  Title
+} from './styles';
+
+export function Resume() {
+  return (
+    <Container>
+      <Header>
+        <Title>Resumo por categoria</Title>
+      </Header>
+
+      <HistoryCard 
+        title='Compras'
+        amount=' 150,00'
+        color='red'
+      /> 
+
+      <HistoryCard 
+        title='Compras'
+        amount=' 150,00'
+        color='red'
+      /> 
+
+    </Container>
+  )
+}
