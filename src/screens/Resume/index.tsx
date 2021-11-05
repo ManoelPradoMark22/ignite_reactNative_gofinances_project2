d93088550a805ea336d23d45b53d87894cb8d63b
Manoel Prado <manoelprado.aecjr@gmail.com>
2021-11-05 15:14:18 -0300

    fix list of hooks error -> set isLoading as true and false in loadData() function

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -46,14 +46,13 @@ interface CategoryData {
 
 export function Resume() {
   
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
 
   const theme = useTheme();
 
   function handleDateChange(action: 'next' | `prev` ) {
-    setIsLoading(true);
     if (action === 'next') {
       setSelectedDate(addMonths(selectedDate, 1));
     }else {
@@ -62,6 +61,7 @@ export function Resume() {
   }
 
   async function loadData() {
+    setIsLoading(true);
     const dataKey = '@gofinances:transactions';
     const response = await AsyncStorage.getItem(dataKey);
     const responseFormatted = response ? JSON.parse(response) : [];
