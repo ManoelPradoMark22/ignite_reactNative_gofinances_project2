81d841292ad96d4e826f734a78edd5e6afe086f1
unknown <manoelprado.aecjr@gmail.com>
2021-11-05 09:36:46 -0300

    fix scrollView in RESUME page

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { VictoryPie } from 'victory-native';
 import { RFValue } from 'react-native-responsive-fontsize';
+
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { useTheme } from 'styled-components';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { HistoryCard } from '../../components/HistoryCard';
 
@@ -52,8 +54,6 @@ export function Resume() {
     .reduce((acumullator : number, expensive : TransactionData) => {
       return acumullator + Number(expensive.amount);
     }, 0);
-
-    console.log(expensiveTotal);
 
     const totalByCategory : CategoryData[] = [];
 
@@ -109,8 +109,13 @@ export function Resume() {
             <Title>Resumo por categoria</Title>
           </Header>
 
-          <Content>
-
+          <Content 
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              paddingHorizontal: 24,
+              paddingBottom: useBottomTabBarHeight(),
+            }}
+          >
             <ChartContainer>
               <VictoryPie
                 data={totalByCategories}
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,5 +1,5 @@
-import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
@@ -29,12 +29,7 @@ export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
 `;
 
-export const Content = styled.ScrollView.attrs({
-  contentContainerStyle: { 
-    flex: 1,
-    padding: 24
-  }
-})``;
+export const Content = styled.ScrollView``;
 
 export const ChartContainer = styled.View`
   width: 100%;
