9656d9595bb2e6ed93b3eac8dda231ff982207f2
unknown <manoelprado.aecjr@gmail.com>
2021-11-05 13:11:56 -0300

    style StatusBar

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
@@ -31,6 +32,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle='light-content' translucent backgroundColor="transparent"/>
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
